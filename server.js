fd6da74ee3c37edb43ae04188524524519fa57b8
Deni-Prasetyo <prasetyodenibudi@gmail.com>
2021-12-15 12:33:31 +0700

    add morgan in the server

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 
 const app = express();
 
 dotenv.config({ path: "config.env" });
 const PORT = process.env.PORT || 5000;
+
+//log request
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.send("CRUD Application");
